442cfb929ef5d4637519ff9f1ffb76fe4eb2abe3
Panda Kuma <luciennoah.soyer@uzh.ch>
2025-04-03 20:59:21 +0200

    # M3 Week 2 (Coder Lunoastro)
    Refactor DTOs to use Lombok annotations for cleaner code

--- src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/team/TeamGetDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/team/TeamGetDTO.java
@@ -1,7 +1,16 @@
 package ch.uzh.ifi.hase.soprafs24.rest.dto.team;
-
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TeamGetDTO {
   
     private Long teamId;
@@ -10,52 +19,4 @@ public class TeamGetDTO {
     private int teamLevel;
     private String teamCode;
     private List<Long> teamMembers;
-
-    public Long getTeamId() {
-        return teamId;
-    }
-
-    public void setTeamId(Long teamId) {
-        this.teamId = teamId;
-    }
-
-    public String getTeamName() {
-        return teamName;
-    }
-
-    public void setTeamName(String teamName) {
-        this.teamName = teamName;
-    }
-
-    public int getTeamXP() {
-        return teamXP;
-    }
-
-    public void setTeamXP(int teamXP) {
-        this.teamXP = teamXP;
-    }
-
-    public int getTeamLevel() {
-        return teamLevel;
-    }
-
-    public void setTeamLevel(int teamLevel) {
-        this.teamLevel = teamLevel;
-    }
-
-    public String getTeamCode() {
-        return teamCode;
-    }
-
-    public void setTeamCode(String teamCode) {
-        this.teamCode = teamCode;
-    }
-
-    public List<Long> getTeamMembers() {
-        return teamMembers;
-    }
-
-    public void setTeamMembers(List<Long> teamMembers) {
-        this.teamMembers = teamMembers;
-    }
 }
