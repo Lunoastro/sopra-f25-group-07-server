111a7ecdf76c374ab7827923544acac8c2c32252
sophiaelena.luther <sophiaelena.luther@uzh.ch>
2025-05-08 14:00:54 +0200

    #250 and #148 some more SonarCloud fixes

--- src/test/java/ch/uzh/ifi/hase/soprafs24/controller/TaskControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/controller/TaskControllerTest.java
@@ -942,8 +942,6 @@ void DELETE_successfulFinishTask_recurringTask_taskRescheduled() throws Exceptio
     updatedTaskDTO.setName("Recurring Task");
     updatedTaskDTO.setIsAssignedTo(null); // Should be unassigned after completion
     
-    //List<TaskGetDTO> updatedTasks = Collections.singletonList(updatedTaskDTO);
-    
     doNothing().when(teamService).validateTeamPaused(token);
     doNothing().when(taskService).validateTaskInTeam(token, taskId);
     when(taskService.getTaskById(taskId)).thenReturn(existingTask);
